fad4b1dae19883dd9575bad0da5894ce36818deb
tuantuana <thuytrang5962@gmail.com>
2025-04-09 01:51:43 +0700

    apinew

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ const to = from; // Thời gian hiện tại
 module.exports = {
     TELEGRAM_BOT_TOKEN: '7910394851:AAG85N-YL1WpQVkVRGuIrjjCx1Fim4s_ZY0',
     TELEGRAM_CHAT_ID: '5710130520',
-    API_KEY: '44eddd64-d23f-4720-8337-17465126b6e1',
+    API_KEY: '8f5b4a93-52d8-4b4b-8f7c-4eacf91ac3c9',
     from,
     to
 };
