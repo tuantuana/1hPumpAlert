47674bcd088c94a764bf22c32fdb357b311199b3
tuantuana <thuytrang5962@gmail.com>
2025-04-10 01:31:31 +0700

    vol1

--- utils/filterBullishAbove10Percent.js
+++ utils/filterBullishAbove10Percent.js
@@ -7,7 +7,7 @@ const filterBullishAbove10Percent = (data) => {
             const buyVolume = c.bv;
             const sellVolume = c.v - c.bv;
             const buyVsSellPercent = ((buyVolume - sellVolume) / sellVolume) * 100;
-            return percentChange >= 5 && buyVsSellPercent >= 10;
+            return percentChange >= 5 && buyVsSellPercent >= 1;
         });
         if (valid.length > 0) {
             filtered[symbol] = valid;
