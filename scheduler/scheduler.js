1cccc85b88b5239b3b39b6571e806d7035fcb6ea
tuantuana <91653958+tuantuana@users.noreply.github.com>
2025-04-11 18:31:55 +0000

    cron50

--- scheduler/scheduler.js
+++ scheduler/scheduler.js
@@ -6,7 +6,7 @@ const fetchAllData = require('../services/fetchAllData');
 const cron = require('node-cron');
 
 module.exports = () => {
-    cron.schedule('47 * * * *', () => {
+    cron.schedule('50 * * * *', () => {
         console.log('⏰ [CRON] Đến phút 47 rồi, đang chạy fetchAllData...');
         fetchAllData();
     });
