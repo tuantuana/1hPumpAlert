4d9d391a8360ced4342f82c2faaf5b6e53e26dff
tuanmcwar <tuanntch1@gmail.com>
2025-08-01 21:32:43 +0700

    5p

--- scheduler/scheduler.js
+++ scheduler/scheduler.js
@@ -6,7 +6,7 @@ const fetchAllData = require('../services/fetchAllData');
 const cron = require('node-cron');
 
 module.exports = () => {
-    cron.schedule('49 * * * *', () => {
+    cron.schedule('54 * * * *', () => {
         console.log('⏰ [CRON] Đến phút 47 rồi, đang chạy fetchAllData...');
         fetchAllData();
     });
